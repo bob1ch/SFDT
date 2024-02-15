dc99c81c651bd35045f15ebc4b5af923f0db6d5e
user1 <none@dev.null>
2024-02-15 17:50:50 +1000

    Some changes in main.py from FastAPI docs

---
+++ FastAPI/main.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+        
+    if model_name.value == ModelName.lenet:
+        return {"model_name": model_name, "message": "LeCNNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q" : q})
+    if not short:
+        item.update(
+            {"description": "strange description from the FastApi docs"}
+        )
+    return item
