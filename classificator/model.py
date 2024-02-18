23ff2293f2589f6707659083f126d952d95c6008
bob1ch <bobich1488@mail.ru>
2024-02-18 18:53:17 +1000

    Added to_vectorize method and updated method fit

--- classificator/model.py
+++ classificator/model.py
@@ -2,11 +2,14 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 class classificator:
 
     def __init__(self, ):
-        clf = DecisionTreeClassifier()
+        self.clf = DecisionTreeClassifier()
+        self.vectorizer = TfidfVectorizer()
 
 
     #temporary method for read data
@@ -17,7 +20,7 @@ class classificator:
     def fit(self, ):
         X, y = self.read_data_spam()
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3) #test_size must be const or var
-        
+        X_train, X_test = self.to_vectorize(X_train, X_test)
         return X_train, X_test, y_train, y_test
 
     def predict(self, ) -> float:
@@ -28,7 +31,9 @@ class classificator:
     
     def get_picture(self, ):
         raise NotImplementedError()
-
+    
+    def to_vectorize(self, X_train: pd.DataFrame, X_test: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+        return self.vectorizer.fit_transform(X_train), self.vectorizer.transform(X_test)
 
 
 
@@ -39,4 +44,4 @@ if __name__ == '__main__':
 
     print(clf)
     print(clf.read_data_spam())
-    print(clf.fit())
+    print(type(clf.fit()))
