ec86fdc67f33e58faffb8333069caf1987f6fed9
bob1ch <bobich1488@mail.ru>
2024-02-20 17:35:00 +1000

    Some changes in api.py but not working yet :(

--- FastAPI/api.py
+++ FastAPI/api.py
@@ -1,118 +1,18 @@
-from typing import Annotated
-
-from enum import Enum
-
-from fastapi import FastAPI, Query, Path, Body
-from pydantic import BaseModel
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
+from fastapi import FastAPI
+from classificator.model import load_model
 
 app = FastAPI()
+model = None
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/")
+async def test():
+    return "i'm fine"
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-        
-    if model_name.value == ModelName.lenet:
-        return {"model_name": model_name, "message": "LeCNNN all the images"}
-    
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.on_event("startup")
+def startup_event():
+    global model
+    model = load_model()
 
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q" : q})
-    if not short:
-        item.update(
-            {"description": "strange description from the FastApi docs"}
-        )
-    return item
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
-
-#Query parameters
-@app.get("/items")
-async def read_items(
-    q: Annotated[
-        str | None,
-        Query(
-            alias="item-query",
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-            max_length=50,
-            pattern="^fixedquery$",
-            deprecated=True,
-            ),
-    ] = None,
-    #hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None
-):
-    """
-    if hidden_query:
-        return {"hidden_query": hidden_query}
-    else:
-        return {"hidden_query": "Not found"}
-    """
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
-#Path parameters
-@app.get("/items_path/{item_id}")
-async def read_items_path(
-    *,
-    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
-    q: str,
-    size: Annotated[float, Query(gt=0, lt=10.5)],
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    return results
-
-@app.put("/items/{items_id}")
-async def update_item(
-    items_id: int,
-    item: Item,
-    user: User,
-    importance: Annotated[int, Body()]
-):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    return results
+@app.get("/test_model")
+def model_test():
+    return model.predict()
